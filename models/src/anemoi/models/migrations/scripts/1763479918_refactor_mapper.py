51d4f45a6b7554164cdc8e881f5ae6abbb1d2e92
jameschappell <james.chappell@metoffice.gov.uk>
2026-02-03 17:04:25 +0000

    Fix: refactor_mapper in case model doesn't have "trainable" features

--- models/src/anemoi/models/migrations/scripts/1763479918_refactor_mapper.py
+++ models/src/anemoi/models/migrations/scripts/1763479918_refactor_mapper.py
@@ -38,12 +38,17 @@ def migrate(ckpt: CkptType) -> CkptType:
     for block in ["encoder", "processor", "decoder"]:
         old_block, new_block = block, f"{block}_graph_provider"
         for layer in update_layers:
-            ckpt["state_dict"][layer.format(block=new_block)] = ckpt["state_dict"].pop(layer.format(block=old_block))
+            old_key = layer.format(block=old_block)
+            new_key = layer.format(block=new_block)
+            # Only update if old key exists and new key doesn't
+            if old_key in ckpt["state_dict"] and new_key not in ckpt["state_dict"]:
+                ckpt["state_dict"][new_key] = ckpt["state_dict"].pop(old_key)
 
     delete_layers = ["model.model.{block}.edge_inc"]
     for block in ["encoder", "processor", "decoder"]:
         for layer in delete_layers:
-            del ckpt["state_dict"][layer.format(block=block)]
+            layer_key = layer.format(block=block)
+            ckpt["state_dict"].pop(layer_key, None) # ignore missing keys
 
     return ckpt
 
